aa716c42277de7d5d907d5d914ce10adf1d0ed94
Allen Zheng <zq95521911@gmail.com>
2018-09-27 15:17:10 +0800

    add unity test framework

---
+++ hello_unity/hello_world.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "unity.h"
+
+
+void setUp(void)
+{
+    printf("Test setup\n");
+    }
+
+void tearDown(void)
+{
+    printf("Test tear down.\n");
+    }
+
+void test_helloworld(void)
+{
+    printf("Hello world.\n");
+    }
